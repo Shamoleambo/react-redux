ac6edd7d3e5d575e34adcf4266651885b4307f73
Tiago Gomes <tidgomes@gmail.com>
2023-03-28 15:33:10 -0300

    create slice for authorization state

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const initialState = { counter: 0, showCounter: true }
+const initialCounterState = { counter: 0, showCounter: true }
+const initialAuthState = { isAuthenticated: false }
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++
@@ -21,8 +22,24 @@ const counterSlice = createSlice({
   }
 })
 
-const store = configureStore({ reducer: { counter: counterSlice.reducer } })
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true
+    },
+    logout(state) {
+      state.isAuthenticated = false
+    }
+  }
+})
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
+})
 
 export default store
 
 export const counterActions = counterSlice.actions
+export const authActions = authSlice.actions
